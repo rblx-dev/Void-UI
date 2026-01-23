2015680c9d1cc7b5d11c9b369de503a28fe687ee
materials not found <130859785+rblx-dev@users.noreply.github.com>
2026-01-23 13:38:12 +0530

    Updated.

--- Void UI.lua
+++ Void UI.lua
@@ -25,7 +25,7 @@ local Window = Rayfield:CreateWindow({
     Discord = {
       Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
       Invite = "GN2McWRFyE", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      RememberJoins = true -- Set this to false to make them join the Discord every time they load it up
    },
 
    KeySystem = true, -- Set this to true to use our key system
@@ -35,8 +35,8 @@ local Window = Rayfield:CreateWindow({
       Note = "The keys can be found in the Discord server.", -- Use this to tell the user how to get a key
       FileName = "what are you doing here, bro", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"VOID"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"https://pastebin.com/raw/9TZ28eMy"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
 
 
 
@@ -45,7 +45,7 @@ local Window = Rayfield:CreateWindow({
 
 Rayfield:Notify({
    Title = "Injected.",
-   Content = "https://discord.gg/GN2McWRFyE",
+   Content = "https://www.youtube.com/@materialsnotfound",
    Duration = 5.0,
    Image = "check",
 })
@@ -203,3 +203,4 @@ local Button = Tab:CreateButton({
    loadstring(game:HttpGet("https://raw.githubusercontent.com/rblx-dev/Void-UI/main/Scripts/Fast%20Animation.lua"))()
    end
 })
+
